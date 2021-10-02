7be146f95ff95eda85b2900b9beb8285314826d6
Charles Weir <797903+charlesweir@users.noreply.github.com>
2021-10-02 17:09:24 +0100

    Stopped accidental double installations.

--- MakeInstaller.cmd
+++ MakeInstaller.cmd
@@ -1,7 +1,8 @@
 @rem Windows Command Script to create ImageAndTableSupport.MSI
 
+@Rem https://serverfault.com/questions/50085/how-do-you-handle-cmd-does-not-support-unc-paths-as-current-directories
 pushd "%~dp0\"
 candle ImageAndTableSupport.wxs
-light ImageAndTableSupport.wixobj
+light -sice:ICE61 ImageAndTableSupport.wixobj
 popd
 @pause
